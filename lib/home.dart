a4e54f9080b88c7f8be364cbcc390a11b59ee31d
saurabhcool001 <parmarsaurabh87@gmail.com>
2019-05-07 18:47:07 +0530

    Fashion 6.0 Drawer Creation with on click list

--- lib/home.dart
+++ lib/home.dart
@@ -1,22 +1,5 @@
 import 'package:flutter/material.dart';
 
-// Stateless Widget
-//class HomePage extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return Container(
-//      color: Colors.white,
-//      child: new Column(
-//        children: <Widget>[
-//          Text("Stateless Widget"),
-//          Icon(Icons.person)
-//        ],
-//      ),
-//    );
-//  }
-//}
-
-//Stateful Widget
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -25,15 +8,90 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Column(
-        children: <Widget>[
-          Text("Stateful Widget"),
-          Icon(Icons.person)
-        ],
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: Colors.red,
+          centerTitle: true,
+          title: Text("Fashion App"),
+          actions: <Widget>[
+            new IconButton(
+                icon: Icon(Icons.search, color: Colors.white),
+                onPressed: () {}),
+            new IconButton(
+                icon: Icon(Icons.shopping_cart, color: Colors.white),
+                onPressed: () {})
+          ]),
+      drawer: new Drawer(
+        child: new ListView(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              accountName: Text("Saurabh"),
+              accountEmail: Text("parmarsaurabh@gmail.com"),
+              currentAccountPicture: GestureDetector(
+                child: new CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  child: Icon(Icons.person, color: Colors.white, size: 50),
+                ),
+              ),
+              decoration: BoxDecoration(color: Colors.red),
+            ),
+
+            //Body part of navigation list
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Home"),
+                leading: Icon(Icons.home),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("My Account"),
+                leading: Icon(Icons.person),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("My Orders"),
+                leading: Icon(Icons.shopping_basket),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Categories"),
+                leading: Icon(Icons.dashboard),
+              ),
+            ),
+            InkWell(
+              onTap: () {},
+              child: ListTile(
+                title: Text("Favorites"),
+                leading: Icon(Icons.favorite),
+              ),
+            ),
+
+            Divider(),
+
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text("Settings"),
+                leading: Icon(Icons.settings),
+              ),
+            ),
+            InkWell(
+              onTap: (){},
+              child: ListTile(
+                title: Text("About"),
+                leading: Icon(Icons.help),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
