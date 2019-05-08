0628badd10db15699e0c5c6b54da88d86cee20e1
saurabhcool001 <parmarsaurabh87@gmail.com>
2019-05-08 15:25:12 +0530

    Fashion 11.0 Product Details Page Design

--- lib/pages/product_details.dart
+++ lib/pages/product_details.dart
@@ -50,14 +50,100 @@ class _ProductDetailsState extends State<ProductDetails> {
           children: <Widget>[
             new Container(
               height: 300.0,
-              child: Container(
-                color: Colors.white,
                 child: GridTile(
+                  child: Container(
+                    color: Colors.white,
                   //To access base class variable use "widget.___"
                     child: Image.asset(widget.productDetailsPicture),
                 ),
+                  footer: new Container(
+                    color: Colors.white70,
+                    child: ListTile(
+                      leading: new Text(widget.productDetailsName,
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16.0),),
+                      title: new Row(
+                        children: <Widget>[
+                          Expanded(
+                              child: new Text("\$${widget.productDetailsOldPrice}",
+                              style: new TextStyle(color: Colors.grey,
+                              decoration: TextDecoration.lineThrough,
+                              fontSize: 14.0),)
+                          ),
+                          Expanded(child: new Text("\$${widget.productDetailsPrice}",
+                          style: new TextStyle(color: Colors.red, fontWeight: FontWeight.bold, fontSize: 16.0),)
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
               ),
-            )
+            ),
+
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: MaterialButton(
+                    onPressed: (){},
+                    color: Colors.white,
+                    textColor: Colors.grey,
+                    elevation: 0.2,
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(child: new Text("Size")),
+                        Expanded(child: new Icon(Icons.arrow_drop_down)),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: MaterialButton(
+                    onPressed: (){},
+                    color: Colors.white,
+                    textColor: Colors.grey,
+                    elevation: 0.2,
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(child: new Text("Color")),
+                        Expanded(child: new Icon(Icons.arrow_drop_down)),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: MaterialButton(
+                    onPressed: (){},
+                    color: Colors.white,
+                    textColor: Colors.grey,
+                    elevation: 0.2,
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(child: new Text("Qty")),
+                        Expanded(child: new Icon(Icons.arrow_drop_down)),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+            //Buy Button
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: MaterialButton(
+                      onPressed: (){},
+                    color: Colors.red,
+                    textColor: Colors.white,
+                    elevation: 0.2,
+                    child: new Text("BUY NOW"),
+                  ),
+                ),
+                
+                new IconButton(icon: Icon(Icons.add_shopping_cart), color: Colors.red, onPressed: (){}),
+                new IconButton(icon: Icon(Icons.favorite_border), color: Colors.red, onPressed: (){})
+              ],
+            ),
+
           ],
         ),
       ),
